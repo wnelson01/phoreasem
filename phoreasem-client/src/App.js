de9a0397404baf11036cfbc4e54f4f36b6b04237
wnelson01 <nelson.wade.nelson@gmail.com>
2022-03-17 23:12:17 +0000

    github action test 2

--- phoreasem-client/src/App.js
+++ phoreasem-client/src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          github action test
+          github action test 2
         </p>
         <a
           className="App-link"
