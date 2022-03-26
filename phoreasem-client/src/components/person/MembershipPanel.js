d1964d86d890219ed913a08ffc1498b3cc87189e
wadenelson <nelsonwa@oregonstate.edu>
2022-03-26 06:56:51 -0400

    editable person name (no submission)

--- phoreasem-client/src/components/person/MembershipPanel.js
+++ phoreasem-client/src/components/person/MembershipPanel.js
@@ -1,45 +1,67 @@
 import React, { useEffect, useState } from "react";
-import { Stack, FormControl, Input, Table, Thead, Tr, Th, Tbody, Button, } from "@chakra-ui/react";
+import {
+  Stack,
+  FormControl,
+  Input,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Button,
+} from "@chakra-ui/react";
 import axios from "axios";
-import MembershipRow from './MembershipRow';
+import MembershipRow from "./MembershipRow";
 
 const MembershipPanel = ({ person }) => {
   const [memberships, setMemberships] = useState([]);
   const [membership, setMembership] = useState([]);
   const loadMemberships = async () => {
-      const response = await axios.get(`https://api.phoreasem.app/membership?person=${person.name}`);
-      await setMemberships(response.data);
-  }
+    const response = await axios.get(
+      `https://api.phoreasem.app/membership?person=${person.name}`
+    );
+    await setMemberships(response.data);
+  };
   useEffect(() => {
-      loadMemberships();
-  }, [memberships])
+    loadMemberships();
+  }, [memberships]);
 
   const addMembership = async () => {
-    const response = await axios.post('https://api.phoreasem.app/membership', {
+    const response = await axios.post("https://api.phoreasem.app/membership", {
       person: person.name,
-      team: membership
+      team: membership,
     });
     const newMemberships = [...memberships, response.data];
     await setMemberships(newMemberships);
-  }
+  };
 
   return (
     <Stack>
       <FormControl>
-        <Input placeholder="team" onChange={(e) => setMembership(e.target.value)} />
+        <Input
+          placeholder="team"
+          onChange={(e) => setMembership(e.target.value)}
+        />
       </FormControl>
-      <Button type='submit' onClick={addMembership}>
+      <Button type="submit" onClick={addMembership}>
         add membership
       </Button>
-      <Table variant='striped' size='sm' id='teams'>
-          <Thead>
-              <Tr>
-                  <Th>Teams</Th>
-              </Tr>
-          </Thead>
-          <Tbody>
-              {memberships.map((membership, i) => <MembershipRow membership={membership} memberships={memberships} setMemberships={setMemberships} key={i} />)}
-          </Tbody>
+      <Table variant="striped" size="sm" id="teams">
+        <Thead>
+          <Tr>
+            <Th>Teams</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {memberships.map((membership, i) => (
+            <MembershipRow
+              membership={membership}
+              memberships={memberships}
+              setMemberships={setMemberships}
+              key={i}
+            />
+          ))}
+        </Tbody>
       </Table>
     </Stack>
   );
--- phoreasem-client/src/components/person/PersonRow.js
+++ phoreasem-client/src/components/person/PersonRow.js
@@ -1,31 +1,50 @@
-import React from 'react'
-import { Td, Text, Tr, useDisclosure, IconButton } from '@chakra-ui/react';
-import { ViewIcon, DeleteIcon } from '@chakra-ui/icons'
-import PersonModal from './PersonModal';
-import axios from 'axios';
+import React from "react";
+import {
+  Editable,
+  EditableInput,
+  EditableTExtarea,
+  EditablePreview,
+  Td,
+  Text,
+  Tr,
+  useDisclosure,
+  IconButton,
+} from "@chakra-ui/react";
+import { ViewIcon, DeleteIcon } from "@chakra-ui/icons";
+import PersonModal from "./PersonModal";
+import axios from "axios";
 
 const PersonRow = ({ person, people, setPeople }) => {
-  const {
-    isOpen,
-    onOpen,
-    onClose
-  } = useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const deletePerson = async () => {
-    await axios.delete(`https://api.phoreasem.app/person/${person.id}`)
-    const newPeople = people.filter(per => per.id != person.id);
+    await axios.delete(`https://api.phoreasem.app/person/${person.id}`);
+    const newPeople = people.filter((per) => per.id != person.id);
     setPeople(newPeople);
-  }
+  };
   return (
     <>
       <Tr>
-        <Td><Text noOfLines={1}>{person.id}</Text></Td>
-        <Td><Text noOfLines={1}>{person.name}</Text></Td>
-        <Td><IconButton icon={<ViewIcon />} onClick={onOpen} /></Td>
-        <Td><IconButton icon={<DeleteIcon />} onClick={deletePerson} /></Td>
-        <Td><PersonModal person={person} onClose={onClose} isOpen={isOpen} /></Td>
+        <Td>
+         <Text noOfLines={1}>{person.id}</Text>
+        </Td>
+        <Td>
+          <Editable defaultValue={person.name}>
+            <EditablePreview />
+            <EditableInput />
+          </Editable>
+        </Td>
+        <Td>
+          <IconButton icon={<ViewIcon />} onClick={onOpen} />
+        </Td>
+        <Td>
+          <IconButton icon={<DeleteIcon />} onClick={deletePerson} />
+        </Td>
+        <Td>
+          <PersonModal person={person} onClose={onClose} isOpen={isOpen} />
+        </Td>
       </Tr>
     </>
-  )
-}
+  );
+};
 
-export default PersonRow
+export default PersonRow;
